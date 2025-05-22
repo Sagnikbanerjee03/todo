9d7b31c5eed3f65f7ed17e777ee838a21ae2559e
Sagnikbanerjee03 <sagnik897@gmail.com>
2025-05-22 17:08:00 +0530

    post and push

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,34 @@
 const express=require("express");
+const {createTodo,updateTodo}=require("./type");
 const app=express();
 
 app.use(express.json());
 
 
 app.post("/todo",function(req,res){
-      
+       const createPayload=req.body;
+       const paresedPayload=createTodo.safeParse(createPayload);
+       if(!paresedPayload.success){
+           res.status(411).json({
+            msg:"you sen this wrong inputs",
+           })
+           return;
+       }
+         
+    })
+
+app.put("/completed",function(req,res){
+       const updatePayload=req.body;
+       const paresedPayload=updateTodo.safeParse(updatePayload);
+        if(!paresedPayload.success){
+           res.status(411).json({
+            msg:"you sen this wrong inputs",
+           })
+           return;
+       }
 })
 
-app.put("/todos",function(req,res){
-
-})
-
-app.get("/completed",function(req,res){
+app.get("/todos",function(req,res){
 
 })
 
